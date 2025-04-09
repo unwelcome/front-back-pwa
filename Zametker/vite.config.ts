a1ace5c2835aeeba7e2817ac094b9e0fbe43fe09
unwelcome <moyapohtaxd@mail.ru>
2025-04-10 02:26:58 +0300

    added manifest, created pwa

--- Zametker/vite.config.ts
+++ Zametker/vite.config.ts
@@ -4,11 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      outDir: 'dist',
+      manifest: {
+        id: '/',
+        name: 'Zametker',
+        short_name: 'ZOV',
+        description: 'Удобные Zametki прямо под рукой',
+        theme_color: '#EBF5FF',
+        background_color: '#EBF5FF', 
+        display: 'standalone',     
+        icons: [
+          {
+            src: 'assets/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'assets/logo128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'assets/loading_scr_landscape.jpg',
+            sizes: '1024x579',
+            form_factor: 'wide',
+          },
+          {
+            src: 'assets/loading_scr_portrait.jpg',
+            sizes: '445x791',
+            form_factor: 'narrow',
+          },
+        ],
+      }
+    })
   ],
   resolve: {
     alias: {
